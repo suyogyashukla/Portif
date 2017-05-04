957ed096166401844d59d48fdff5aeea68f3e3b3
Suyogya Shukla <suyogya.shukla09@gmail.com>
2017-05-05 01:06:54 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);
+function child_enqueue_styles() {
+    $parent_style = 'parent-style';
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+}
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+         update_option( 'theme_mods_' . get_template(), $value );
+         return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
+
+	require get_template_directory() . '/inc/customizer.php';
