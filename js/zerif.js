f3c1d263adcf51b02aeca5ba47bd26fc22c9ec64
Suyogya Shukla <suyogya.shukla09@gmail.com>
2017-05-05 14:03:51 +0530

    Fixed syntax error

--- js/zerif.js
+++ js/zerif.js
@@ -1,1 +1,11 @@
-jQuery(document).ready(function($) {  $(window).scroll(function() {      var nav = $('#main-nav');      var top = 200;      if ($(window).scrollTop() >= top) {          nav.addClass('navbar-shadow');      } else {          nav.removeClass('navbar-shadow');      }  });
+jQuery(document).ready(function($) {
+  $(window).scroll(function() {
+      var nav = $('#main-nav');
+      var top = 200;
+      if ($(window).scrollTop() >= top) {
+          nav.addClass('navbar-shadow');
+      } else {
+          nav.removeClass('navbar-shadow');
+      }
+  });
+});
